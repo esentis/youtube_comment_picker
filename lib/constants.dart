e6131fcb420b8338b922d06c66e6d7188b509e9f
esentis <52862600+esentis@users.noreply.github.com>
2023-01-16 00:16:12 +0200

    💄 Adds constant inputdecoration

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 
 Color kColorRedYtb = const Color(0xffE83020);
+
+InputDecoration kInputDecoration({required String labeText}) => InputDecoration(
+      focusColor: Colors.red,
+      labelStyle: TextStyle(
+        color: kColorRedYtb.withOpacity(0.5),
+      ),
+      label: Text(labeText),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(
+          color: kColorRedYtb.withOpacity(0.3),
+          width: 2,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(
+          color: kColorRedYtb,
+          width: 3,
+        ),
+      ),
+    );
