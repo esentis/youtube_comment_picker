e32b5bb9adf665b02b094415e0daaec16d5a5085
esentis <52862600+esentis@users.noreply.github.com>
2023-02-03 01:01:56 +0200

    ✅ Updates dotenv filename

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:youtube_comment_picker/screens/landing_screen.dart';
 
 Future<void> main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: 'dotenv');
   runApp(const MyApp());
 }
 
