50c8ef0781619f5df9f0c668d96af58a207d7f82
esentis <52862600+esentis@users.noreply.github.com>
2024-06-24 15:47:25 +0300

    ♻️ Refactor VideoInfo widget to include thumbnail image and play button

--- lib/widgets/video_info.dart
+++ lib/widgets/video_info.dart
@@ -1,54 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:image_network/image_network.dart';
 import 'package:intl/intl.dart';
 import 'package:string_extensions/string_extensions.dart';
+import 'package:youtube_comment_picker/constants.dart';
 import 'package:youtube_comment_picker/models/video_information.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class VideoInfo extends StatelessWidget {
   const VideoInfo({
     super.key,
     required this.videoInfo,
+    required this.ytbController,
   });
 
   final VideoInformation? videoInfo;
-
+  final YoutubePlayerController? ytbController;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 30.0),
-      child: Column(
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            videoInfo!.title!,
-            textAlign: TextAlign.left,
-            style: const TextStyle(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => Dialog(
+                    child: YoutubePlayer(
+                      controller: ytbController!,
+                    ),
+                  ),
+                );
+              },
+              child: Stack(
+                children: [
+                  ImageNetwork(
+                    image: videoInfo!.thumbnail!,
+                    width: 150,
+                    height: 100,
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    child: Icon(
+                      Icons.play_circle,
+                      color: kColorRedYtb,
+                      size: 50,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           const SizedBox(
-            height: 5,
+            width: 20,
           ),
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                '${NumberFormat('#,###,000').format(videoInfo?.viewCount.toInt())} views',
+              SelectableText(
+                videoInfo!.title!,
                 textAlign: TextAlign.left,
                 style: const TextStyle(
+                  fontSize: 17,
                   fontWeight: FontWeight.bold,
-                  fontSize: 15,
                   color: Colors.white,
                 ),
               ),
-              Text(
-                '   ${DateFormat('MMM d, yyyy').format(videoInfo!.publishedAt!)}',
-                textAlign: TextAlign.left,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${NumberFormat('#,###,000').format(videoInfo?.viewCount.toInt())} views',
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    '   ${DateFormat('MMM d, yyyy').format(videoInfo!.publishedAt!)}',
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
