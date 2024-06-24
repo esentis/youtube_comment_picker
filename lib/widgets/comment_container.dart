3c99c566f7fef246ebee2bd4b7570fad32422850
esentis <52862600+esentis@users.noreply.github.com>
2024-06-24 21:52:35 +0300

    ♻️ Refactor comment_container.dart to use ImageNetwork widget for improved appearance and functionality

--- lib/widgets/comment_container.dart
+++ lib/widgets/comment_container.dart
@@ -1,11 +1,10 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:image_network/image_network.dart';
 import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
 import 'package:substring_highlight/substring_highlight.dart';
 import 'package:youtube_comment_picker/constants.dart';
 import 'package:youtube_comment_picker/models/comment.dart';
-import 'package:youtube_comment_picker/services/youtube_service.dart';
 
 class CommentWidget extends StatelessWidget {
   const CommentWidget({
@@ -19,119 +18,114 @@ class CommentWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: const Color(0xff20262E),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          onTap: () {
-            log.f(comment.toJson());
-          },
-          key: ValueKey(
-            comment.authorProfileImageUrl!,
-          ),
-          leading: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () {
-                kLaunchUrl(
-                  comment.authorChannel ?? '',
-                );
-              },
-              child: ExtendedImage.network(
-                comment.authorProfileImageUrl!,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        onTap: () {
+          kLaunchUrl(
+            comment.authorChannel ?? '',
+          );
+        },
+        key: ValueKey(
+          comment.authorProfileImageUrl!,
+        ),
+        leading: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              kLaunchUrl(
+                comment.authorChannel ?? '',
+              );
+            },
+            child: SizedBox(
+              height: 50,
+              width: 50,
+              child: ImageNetwork(
+                image: comment.authorProfileImageUrl!,
                 height: 50,
                 width: 50,
-                fit: BoxFit.fill,
-                shape: BoxShape.circle,
-                loadStateChanged: (state) {
-                  if (state.extendedImageLoadState == LoadState.failed) {
-                    return Image.network(
-                      'https://i.imgur.com/qV26MhU.png',
-                    );
-                  }
-                  if (state.extendedImageLoadState == LoadState.loading) {
-                    return LottieBuilder.asset(
-                      'assets/loading.json',
-                      height: 100,
-                    );
-                  }
-                  return null;
-                },
+                fitAndroidIos: BoxFit.fill,
+                onLoading: LottieBuilder.asset(
+                  'assets/loading.json',
+                  height: 100,
+                ),
+                onError: Image.network(
+                  'https://i.imgur.com/qV26MhU.png',
+                ),
                 borderRadius: const BorderRadius.all(
                   Radius.circular(30.0),
                 ),
               ),
             ),
           ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SubstringHighlight(
-                textAlign: TextAlign.start,
-                text: comment.text!,
-                term: highlightText,
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-                textStyleHighlight: TextStyle(
-                  color: kColorRedYtb,
-                  fontSize: 16,
-                ),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SubstringHighlight(
+              textAlign: TextAlign.start,
+              text: comment.text!,
+              term: highlightText,
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
-              const SizedBox(
-                height: 10,
+              textStyleHighlight: TextStyle(
+                color: kColorRedYtb,
+                fontSize: 16,
               ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.thumb_up,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(width: 10),
-                  Text(
-                    comment.likeCount!.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 13,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          title: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 8.0,
             ),
-            child: Row(
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
               children: [
+                const Icon(
+                  Icons.thumb_up,
+                  color: Colors.white,
+                ),
+                const SizedBox(width: 10),
                 Text(
-                  comment.authorName!,
+                  comment.likeCount!.toString(),
                   style: const TextStyle(
                     color: Colors.white,
-                    fontSize: 14,
+                    fontSize: 13,
                     fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  DateFormat.yMMMd().format(
-                    comment.createdAt!,
-                  ),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
                   ),
                 ),
               ],
             ),
+          ],
+        ),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+          ),
+          child: Row(
+            children: [
+              Text(
+                comment.authorName!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                DateFormat.yMMMd().format(
+                  comment.createdAt!,
+                ),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
+              ),
+            ],
           ),
         ),
       ),
